8585089760128156441c5b477cd49bd5e62845b4
Kanghyeok Jie <hek1850@naver.com>
2021-03-10 15:41:38 +0900

    update!

--- for.py
+++ for.py
@@ -1,6 +1,41 @@
-# 백준 27138146번
+# 백준 2739번
 
 N = int(input())
 
 for i in range(1 , 10):
     print(N, '*', i, "=", N*i)
+
+# 백준 10950번 
+
+T = int(input())
+
+for i in range(T):
+    A, B = map(int, input().split())
+    print(A+B)
+
+# 백준 8393번
+
+n =  int(input())
+
+for i in range(1,n):
+    if (1 >= n <= 10000):
+        break
+    n = n + i
+print(n)
+
+# 백준 15552번
+
+import sys
+
+T = int(input())
+
+for i in range(T):
+    A, B = map(int, sys.stdin.readline().split())
+    print(A+B)
+
+# 백준 2741번
+
+N = int(input())
+
+for i in range(1, N+1):
+    print(i)
--- memo.py
+++ memo.py
@@ -1,8 +1,4 @@
-H, M = map(int, input().split())
+N = int(input())
 
-if (M > 44):
-    print(H, M-45)
-elif (M < 45 and H > 0):
-    print(H-1, M+15)
-else:
-    print(23, M+15)
+for i in range(N, 0, -1):
+    print(i)
