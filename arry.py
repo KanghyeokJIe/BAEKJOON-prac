96cd72cd57fb5727142369e0803c228fd5086f7d
Kanghyeok Jie <hek1850@naver.com>
2021-03-14 14:25:45 +0900

    update

--- arry.py
+++ arry.py
@@ -7,3 +7,21 @@ print('{} {}'.format(min(A),max(A)))
 
 # 백준 2562번
 
+A = []
+
+for i in range(9):
+    A.append(int(input()))
+
+print(max(A))
+print(A.index(max(A))+1)
+
+# 백준 2577번 
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+result = list(str(a * b * c))
+
+for i in range(10):
+    print(result.count(str(i)))
--- memo.py
+++ memo.py
@@ -1,2 +1,8 @@
-a = list(map(int, input().split()))
+a = int(input())
+b = int(input())
+c = int(input())
 
+result = list(str(a * b * c))
+
+for i in range(10):
+    print(result.count(str(i)))
