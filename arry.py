ecf9cdaf8bf0348a71edcc71d00a52fb4f276798
Kanghyeok Jie <hek1850@naver.com>
2021-03-16 14:43:53 +0900

    update!

--- arry.py
+++ arry.py
@@ -25,3 +25,29 @@ result = list(str(a * b * c))
 
 for i in range(10):
     print(result.count(str(i)))
+
+# 백준 3052번
+
+a = []
+
+for i in range(10):
+    n = int(input())
+    a.append(n % 42)
+
+a = set(a)
+print(len(a))
+
+# 백준 1546번
+
+a = int(input())
+b = list(map(int, input().split()))
+b_max = max(b)
+
+for i in range(a):
+    b[i] = b[i] / b_max * 100
+
+avg = sum(b) / a
+print('{}'.format(avg))
+
+# 백준 8958번
+
--- memo.py
+++ memo.py
@@ -1,8 +1,13 @@
-a = int(input())
-b = int(input())
-c = int(input())
+N = int(input())
 
-result = list(str(a * b * c))
-
-for i in range(10):
-    print(result.count(str(i)))
+for i in range(N):
+    a = input()
+    score = 0
+    sumScore = 0
+    for j in a:
+        if j == 'O':
+            score += 1
+        else:
+            score = 0
+        sumScore += score
+    print(sumScore)
