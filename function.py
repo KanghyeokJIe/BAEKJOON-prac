61903e426ba5ce02833ce806816512096b93e333
Kanghyeok Jie <hek1850@naver.com>
2021-03-18 11:22:13 +0900

    update!

---
+++ function.py
@@ -0,0 +1,7 @@
+# 백준 15596번 
+
+def solve(a):
+    return sum(a)
+
+# 백준 4673번
+
--- memo.py
+++ memo.py
@@ -1,12 +1,4 @@
-c = int(input())
+def d(n):
+    return (n + (n // 10) + (n % 10))
 
-for i in range(c):
-    result = list(map(int, input().split()))
-    avg = sum(result[1:]) / result[0]
-    count = 0
-    for score in result[1:]:
-        if score > avg:
-            count += 1
-    rate = count / result[0] * 100
-
-    print(str('%.3f' % round(count / result[0] * 100, 3)) + '%')
+print()
